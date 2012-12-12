7ad3b9a29f69f5c60323cde1158b305056b67114
Mike Mitterer <office@mikemitterer.at>
2012-12-12 10:23:11 +0100

    Changed Screenshot-Collage for GitHub

--- gen/at/mikemitterer/tutorial/fragments/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package at.mikemitterer.tutorial.fragments;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
